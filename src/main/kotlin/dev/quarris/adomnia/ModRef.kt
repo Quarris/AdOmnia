76b8cabda7a21b5084838a86223b24d9c0e12435
Quarris <quarris@live.co.uk>
2022-05-08 00:31:15 +0100

    Initial commit

---
+++ src/main/kotlin/dev/quarris/adomnia/ModRef.kt
@@ -0,0 +1,12 @@
+package dev.quarris.adomnia
+
+import net.minecraft.resources.ResourceLocation
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+object ModRef {
+    const val ID = "adomnia"
+    val LOGGER: Logger = LogManager.getLogger(ID)
+
+    fun res(name: String): ResourceLocation = ResourceLocation(ID, name)
+}
---
+++ src/main/kotlin/dev/quarris/adomnia/ModRoot.kt
@@ -0,0 +1,8 @@
+package dev.quarris.adomnia
+
+import net.minecraftforge.fml.common.Mod
+
+@Mod(ModRef.ID)
+object ModRoot {
+
+}
